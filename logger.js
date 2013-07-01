dac5f984f3cfa4a84751fec1210ebe565623ab9c
Willian Carvalho <willian.carvalho@totvs.com.br>
2013-07-01 11:13:26 -0300

    portuguese texts translated to english additional comments

--- logger.js
+++ logger.js
@@ -1,19 +1,23 @@
 var
-	write = require('fs').appendFile,
-	doLog = require('util').log
-
+	write = require('fs').appendFile
 ;
 
+/*
+Returns the filename to be used for the logger
+Log file changes every day, as the follows:
+	- 201371.log
+	- 201372.log
+	And so on, whenever a new day arrives
+*/
 function getDate() {
 	var d = new Date();
 	return d.getFullYear() + '' + (d.getMonth()+1) + '' + d.getDate() + '.log';
 }
 
 exports.log = function(str) {
-	console.trace('inicio da funcao log')
 	write(getDate(), new Date() + ' - ' + str + '\r\n', function(err) {
 		if(err) {
-			console.log("Ocorreu um erro ao tentar gravar o log" + err);
+			console.log("An error occurred while trying to write the log: " + err);
 		}
 	});
 };
